a8a750f03da999a50b66daad57c2e433ac41dd53
CarsonAnderson1 <andersonc20@spu.edu>
2023-05-17 13:55:43 -0700

    updated

--- library.js
+++ library.js
@@ -1,13 +1,32 @@
+window.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#searchButton").addEventListener("click", function () {
+       // Get values from drop-downs
+       const topic = document.getElementById("searchInput").value;
+       // Get and display book
+       newLibrary = new httpLibrary("https://jsonplaceholder.typicode.com/posts");
+       newLibrary.get(topic);
+    });
+ });
 class httpLibrary{
     constructor(baseURL){
         this.baseURL = baseURL;
     }
     async get(destination){
         try{
-            const response = await fetch(`${this.baseURL}${destination}`)
+            const response = await fetch(this.baseURL);
             const data = await response.json();
+            let html = '<ol>';
+            for (let line of data) {
+                if(line.title == destination){
+                    html += '<li>' + line.title + '</li>' + '<li>' + line.body + '</li>';
+                }
+             }
+             html += "</ol>";
+
+            document.getElementById("booksDisplay").innerHTML = html;
             return data;
         } catch (error) {
+            document.getElementById("booksDisplay").innerHTML = "Topic '" + destination + "' not found";
             throw new Error(`GET request failed to ${endpoint} failed`);
         }
     }
