013136aa2c3d6c21656d75237c5df721e7014627
CarsonAnderson1 <andersonc20@spu.edu>
2023-05-17 21:48:22 -0700

    recent

--- library.js
+++ library.js
@@ -1,34 +1,46 @@
 window.addEventListener("DOMContentLoaded", function () {
+    newLibrary = new httpLibrary();
     document.querySelector("#searchButton").addEventListener("click", function () {
        // Get values from drop-downs
        const topic = document.getElementById("searchInput").value;
        // Get and display book
-       newLibrary = new httpLibrary("https://jsonplaceholder.typicode.com/posts");
-       newLibrary.get(topic);
+       newLibrary.get(topic)
+       .then(responseData => ShowResponse(responseData))
+       .catch(err => ShowError(err));
     });
  });
-class httpLibrary{
-    constructor(baseURL){
-        this.baseURL = baseURL;
+function ShowResponse(responseData){
+    let html = "<ol style = 'list-style:none'/>";
+    if(Array.isArray(responseData)){
+        responseData.forEach(book => {
+            html += `<li>${book.id}. ${book.title}  -  ${book.body}</li>`;
+        })
     }
+    else{
+        html += `<li>User ${responseData.id} -  ${responseData.body}</li>`
+    }
+    document.getElementById("booksDisplay").innerHTML = html;
+}
+class httpLibrary{
     async get(destination){
-        try{
-            const response = await fetch(this.baseURL);
-            const data = await response.json();
-            let html = '<ol>';
-            for (let line of data) {
-                if(line.title == destination){
-                    html += '<li>' + line.title + '</li>' + '<li>' + line.body + '</li>';
+        const theRequest = new Promise((resolve,reject) => {
+            const requestOption = {
+                method: "GET",
+                headers: {"content-type": "application\json"}
+            }
+            fetch(destination, requestOption)
+            .then(response => {
+                if(response.ok){
+                    return response.json();
                 }
-             }
-             html += "</ol>";
-
-            document.getElementById("booksDisplay").innerHTML = html;
-            return data;
-        } catch (error) {
-            document.getElementById("booksDisplay").innerHTML = "Topic '" + destination + "' not found";
-            throw new Error(`GET request failed to ${endpoint} failed`);
-        }
+                else{
+                    throw new Error(response.status);
+                }
+            })
+            .then(responseData => resolve(responseData))
+            .catch(err => reject(err));
+        })   
+        return theRequest;
     }
     async post(destination, data){
         //fix
